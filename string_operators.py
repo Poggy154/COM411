df3c1203803926673db12f27129ac051216e7306
Liam Pogson <5pogsl81@solent.ac.uk>
2021-10-01 15:47:10 +0100

    string_operators.py

---
+++ string_operators.py
@@ -0,0 +1,12 @@
+# a program to improve beeps health
+print("Please enter the number of lives")
+lives=int(input())
+
+print("please enter the energy level")
+level=int(input())
+print("Please enter the shield level")
+s_level= int(input())
+print("health has been set\n")
+print(f"lives: {'♥' * lives } ")
+print(f"level: {'♦' * level}")
+print(f"shield: {'♦' * s_level}")
