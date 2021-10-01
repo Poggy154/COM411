ca0dd8ff615e91d696257efa5ba6543022e880fa
Liam Pogson <5pogsl81@solent.ac.uk>
2021-10-01 15:02:51 +0100

    escape_characters.py

--- multiline_message.py
+++ multiline_message.py
@@ -1,9 +1,9 @@
 # Display message to the standard output
 print("System Failure Imminent!")
-print("System reboot has been initiated...")
+print("System reboot has been initiated...\n")
 
 print("...rebooting sensory system")
 print("rebooting output motors")
-print("...rebooting hover engine")
+print("...rebooting hover engine\n")
 
 print("System online.")
