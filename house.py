b6a4bb10b8289d5c66a356f5b88e10b7b12c9f26
unknown <liampogson6@gmail.com>
2021-10-05 16:39:57 +0100

    rows

---
+++ house.py
@@ -0,0 +1,7 @@
+print("please enter number of rows:")
+rows = int(input())
+print("please enter number of columns:")
+cols = int(input())
+for row in range(rows):
+    for col in range(cols):
+        print("rows")
