3dbad756efd9d9ba6255804fa8c5f80eb2e6f143
unknown <liampogson6@gmail.com>
2021-10-05 16:30:52 +0100

    if

---
+++ float.py
@@ -0,0 +1,13 @@
+for count in range(5):
+    print("please enter a number")
+    user_number = float(input())
+
+
+
+count = 0
+
+while count < 5:
+    print("Please entere a number")
+    user_number = float(input())
+
+count = count + 1
