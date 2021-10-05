59c2c1a2bab81471559b5554e5fd493eb424fbcc
unknown <liampogson6@gmail.com>
2021-10-05 16:00:58 +0100

    if_+_Add_statements.py

--- if.py
+++ if.py
@@ -7,3 +7,19 @@ elif age >= 13:
     print("you are a teenager")
 else:
     print("you are a child")
+
+
+print("is it sunny?")
+is_sunny = input().lower()
+
+print("is it breezy?")
+is_breezy = input().lower()
+
+if is_sunny and is_breezy == "yes":
+    print("The clothes are dry")
+elif is_sunny == "yes" and is_breezy == "no":
+    print("the clothes are going to take a while to dry")
+else:
+    print("the clothes are not dry")
+
+
