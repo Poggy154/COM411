91d3f0b22b9568466c94ef105513f5fab48b980b
Liam Pogson <5pogsl81@solent.ac.uk>
2021-10-08 14:48:13 +0100

    if_else_statements

---
+++ If_else.py
@@ -0,0 +1,9 @@
+print("please enter the activity you want to perform")
+activity = input()
+if activity == "calculate":
+    print("performing calculations.....\n")
+
+else:
+    print("Performing activity...\n")
+
+print("activity completed!")
