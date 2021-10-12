7358d2a52d9b653f5c1334fb72b981ad8e622e74
unknown <liampogson6@gmail.com>
2021-10-12 16:05:23 +0100

    get_name_function

--- get_name.pyu.py
+++ get_name.pyu.py
@@ -2,4 +2,11 @@ def get_name():
     print("Please enter your name")
     name = input()
     return name
-print(got_name())
+
+def display_name():
+    print(f"***{get_name()} ***")
+
+name = get_name()
+display_name()
+
+print(get_name())
