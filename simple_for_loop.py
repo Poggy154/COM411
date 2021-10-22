33679c1d4939411e70ded2c7eebbfd3de31d2e0b
Liam Pogson <5pogsl81@solent.ac.uk>
2021-10-22 15:45:48 +0100

    simple for loop

---
+++ simple_for_loop.py
@@ -0,0 +1,16 @@
+print("how many mountains should i display")
+mountains = int(input())
+print("Displaying...")
+
+for mountain in range(mountains):
+    print("""
+    
+              __
+            /   \\_
+           /^      \\
+          /  ^       \\_
+        _/ ^ ^        ^\\
+        /  ^       ^     \\
+        
+        
+""")
