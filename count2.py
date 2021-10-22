740b46738495af334e78276c5d45a8bbc88b1ff7
Liam Pogson <5pogsl81@solent.ac.uk>
2021-10-22 15:16:55 +0100

    ascii2

--- count2.py
+++ count2.py
@@ -8,4 +8,4 @@ print()
 while cables_avoided < cables_to_avoid:
     print("avoiding...", end="\n")
     cables_avoided = cables_avoided + 1
-    print(f"Done{cables_avoided} live cables avoided!")
+    print(f"Done {cables_avoided} live cables avoided!")
