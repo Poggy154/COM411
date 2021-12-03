8a3e8393346fd97445f4171b18025370ec944be6
Liam Pogson <5pogsl81@solent.ac.uk>
2021-12-03 14:58:41 +0000

    function nesting

---
+++ function_with_nesting.py
@@ -0,0 +1,10 @@
+def identify():
+    print("What lies before us?")
+    response = input()
+
+    if response == "a large boulder":
+        print("WE NEED TO RUN!!")
+    else:
+        print("We'll be alright")
+
+identify()
