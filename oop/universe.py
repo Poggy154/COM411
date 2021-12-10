cd032a0d749b619a0a3336f53596a8776529cb30
Liam Pogson <5pogsl81@solent.ac.uk>
2021-12-10 15:49:18 +0000

    universe

---
+++ oop/universe.py
@@ -0,0 +1,51 @@
+from planet import Planet
+from robot import Robot
+from human import Human
+import matplotlib.pyplot as plt
+
+
+import random
+class Universe:
+
+    def __init__(self):
+        self.planets=[]
+
+    def __repr__(self):
+        return f"universe contains {len(self.planets)} planets."
+
+    def __str__(self):
+        return f"The universe contains {len(self.planets)} planets,"
+
+
+    def generate(self):
+        planet = Planet()
+
+        for index in range(random.randint(1, 10)):
+            robot = Robot(f"Robot{index}")
+            planet.add_human(human)
+
+        self.planets.append(planet)
+
+    def show_population(self):
+        num_subplots = len(self.planets)
+
+        fig, axs = plt.subplots(1, num_subplots)
+
+        for index in range(num_subplots):
+            planet = self.planets[index]
+            num_humans = len(planet.inhabitants['humans'])
+            num_robots = len(planet.inhabitants['robots'])
+
+            if num_subplots == 1:
+                axs.bar([1, 2], [num_humans, num_robots])
+            else:
+                axs[index].bar([1, 2], [num_humans, num_robots])
+
+        plt.tight_layout()
+        plt.show()
+
+if __name__ == "__main__":
+    universe = Universe()
+    universe.generate()
+    universe.show_populations()
+
